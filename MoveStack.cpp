f7b23ec749cef5248d7203a706ba5c7cf9e95982
APNumbz <124722278+APNumbz@users.noreply.github.com>
2024-04-30 22:31:17 -0500

    Create MoveStack.cpp

---
+++ MoveStack.cpp
@@ -0,0 +1,45 @@
+#include <cassert>
+using namespace std;
+
+#include "MoveStack.h"
+
+
+MoveStack::MoveStack() { 
+
+}
+
+
+MoveStack::~MoveStack() {
+
+}
+
+//****************************************************
+// Member function getsize returns a count of the    *
+// number of elements on the stack.                  *
+//****************************************************
+int MoveStack::getSize() {
+
+ }
+
+//****************************************************
+// Member function top returns the value at the top  *
+// of the stack (does not remove it).                *
+//****************************************************
+Move MoveStack::top() {
+
+}
+//*************************************************
+// Member function push pushes the argument onto  *
+// the stack.                                     *
+// move: the Move item to be pushed               *
+//*************************************************
+void MoveStack::push(Move move) {
+
+};
+//****************************************************
+// Member function pop removes the value at the top  *
+// of the stack, but returns nothing.                *
+//****************************************************
+void MoveStack::pop() {
+
+}
